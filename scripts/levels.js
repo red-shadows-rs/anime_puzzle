940da0823721e09e03c889b8345517a6611999e5
NightcoreAT <oussamakatalonya1@gmail.com>
2025-07-10 22:44:10 +0100

    ✨| UPLOADED

---
+++ scripts/levels.js
@@ -0,0 +1,131 @@
+const levels = [
+  {
+    id: 1,
+    name: "نيشيكا",
+    width: 4,
+    height: 4,
+    image: "assets/puzzles/nichika-smile.png",
+    difficulty: "سهل"
+  },
+  {
+    id: 2,
+    name: "فتاة الموسيقى",
+    width: 5,
+    height: 5,
+    image: "assets/puzzles/guitar_girl.jpg",
+    difficulty: "سهل"
+  },
+  {
+    id: 3,
+    name: "فتاة الموضة",
+    width: 4,
+    height: 4,
+    image: "assets/puzzles/fasion.jpg",
+    difficulty: "سهل"
+  },
+  {
+    id: 4,
+    name: "بورتريه حالمة",
+    width: 5,
+    height: 5,
+    image: "assets/puzzles/dreamy_girl.jpg",
+    difficulty: "متوسط"
+  },
+  {
+    id: 5,
+    name: "أنيقة في الطبيعة",
+    width: 6,
+    height: 6,
+    image: "assets/puzzles/natural_beauty_girl.jpg",
+    difficulty: "متوسط"
+  },
+  {
+    id: 6,
+    name: "زهرة دافئة",
+    width: 8,
+    height: 8,
+    image: "assets/puzzles/beauty-crown-flower.jpg",
+    difficulty: "صعب"
+  },
+  {
+    id: 7,
+    name: "أميرة الغابة",
+    width: 8,
+    height: 8,
+    image: "assets/puzzles/forest_princess.jpg",
+    difficulty: "صعب"
+  },
+  {
+    id: 8,
+    name: "همسات الأزهار",
+    width: 8,
+    height: 8,
+    image: "assets/puzzles/flower_whispers.jpg",
+    difficulty: "صعب"
+  },
+  {
+    id: 9,
+    name: "همسات السكون",
+    width: 8,
+    height: 8,
+    image: "assets/puzzles/whispers_of_serenity.png",
+    difficulty: "خبير"
+  },
+  {
+    id: 10,
+    name: "ظل القمر",
+    width: 6,
+    height: 6,
+    image: "assets/puzzles/moon_shadow.png",
+    difficulty: "متوسط"
+  },
+  {
+    id: 11,
+    name: "حارسة النجوم",
+    width: 6,
+    height: 6,
+    image: "assets/puzzles/cosmic_star_maiden.png",
+    difficulty: "صعب"
+  },
+  {
+    id: 12,
+    name: "الرسامة الحالمة",
+    width: 8,
+    height: 8,
+    image: "assets/puzzles/dreamy_artist.png",
+    difficulty: "خبير"
+  },
+  {
+    id: 13,
+    name: "سيدة الفراشات",
+    width: 5,
+    height: 5,
+    image: "assets/puzzles/butterfly_lady.jpg",
+    difficulty: "متوسط"
+  },
+  {
+    id: 14,
+    name: "روح الطبيعة",
+    width: 6,
+    height: 6,
+    image: "assets/puzzles/nature_spirit.jpg",
+    difficulty: "صعب"
+  },
+  {
+    id: 15,
+    name: "ساحرة الغابة",
+    width: 6,
+    height: 6,
+    image: "assets/puzzles/forest_witch.jpg",
+    difficulty: "خبير"
+  },
+  {
+    id: 16,
+    name: "همسات النور",
+    width: 6,
+    height: 6,
+    image: "assets/puzzles/lunaria.jpg",
+    difficulty: "خبير"
+  }
+];
+  
